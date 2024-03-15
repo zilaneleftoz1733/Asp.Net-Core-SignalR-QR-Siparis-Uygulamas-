973a6c3fc0028a706ae07357abd0c5aa8ce8ff2e
ZİLAN ELEFTOZ <lzilaneleftozl@gmail.com>
2024-03-15 06:18:07 +0300

    Theme-Preparation

---
+++ SignalRProject/SignalRWebUI/ViewComponents/LayoutComponents/_LayoutHeaderPartialComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace SignalRWebUI.ViewComponents.LayoutComponents
+{
+    public class _LayoutHeaderPartialComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke() //buranın viewcomponent oldugunu bildirmiş olduk
+        {
+            return View();
+        }
+    }
+}
---
+++ SignalRProject/SignalRWebUI/ViewComponents/LayoutComponents/_LayoutSidebarComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace SignalRWebUI.ViewComponents.LayoutComponents
+{
+	public class _LayoutSidebarComponentPartial : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
