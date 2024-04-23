94d5ed245c1eaf0530f8c8b975548fe1337edcaf
ZİLAN ELEFTOZ <lzilaneleftozl@gmail.com>
2024-04-23 15:33:17 +0300

    Added-SignalR-Library

--- SignalRProject/SignalRApi/Program.cs
+++ SignalRProject/SignalRApi/Program.cs
@@ -4,12 +4,23 @@ using SignalR.DataAccessLayer.Abstract;
 using SignalR.DataAccessLayer.Concrete;
 using SignalR.DataAccessLayer.EntityFramework;
 using SignalR.EntityLayer.Entities;
+using SignalRApi.Hubs;
 using System.Reflection;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.AllowAnyHeader()
+            .AllowAnyMethod()
+            .SetIsOriginAllowed((Host) => true)
+            .AllowCredentials();
+    });
+});
+
 builder.Services.AddSignalR();
 
 builder.Services.AddDbContext<SignalRContext>();
@@ -58,11 +69,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHub<SignalRHub>("/signalrhub");
 
 app.Run();
